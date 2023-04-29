c53037c8ca1350a106f112542aa2c9f6fb1054f1
SkeleByte <74297927+agsurfer2@users.noreply.github.com>
2023-04-29 21:39:39 +0800

    cool title text animation

--- hackertext.js
+++ hackertext.js
@@ -1,22 +1,25 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-document.querySelector("h1").onmouseover = event => {
+window.onload = () => {
+  const headings = document.querySelectorAll("[data-auto-activate-effect='true'][data-value]");
+
+  headings.forEach((heading) => {
     let iterations = 0;
-    
-    const interval = ( () => {
-        event.target.innerText = event.target.innerText.split("").map((letter, index) => {
 
-            if(index < iterations) {
-                return event.target.dataset.value[index];
-            }
-            
-            return letters[Math.floor(Math.random() * letters.length)]
-        }).join("");
+    const interval = setInterval(() => {
+      heading.innerText = heading.dataset.value
+        .split("")
+        .map((letter, index) => {
+          if (index < iterations) {
+            return heading.dataset.value[index];
+          }
+          return letters[Math.floor(Math.random() * letters.length)];
+        })
+        .join("");
 
+      if (iterations >= heading.dataset.value.length) clearInterval(interval);
 
-        if(iterations >= event.target.dataset.value.length) clearInterval(interval);
-
-        iterations += 1 / 5;
+      iterations += 1 / 3;
     }, 30);
-    
-}
+  });
+};
