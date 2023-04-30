7bb305bd220d0ab3993fb1eaf8e1779030c848d6
Skelebyte <74297927+Skelebyte@users.noreply.github.com>
2023-04-30 20:54:56 +0800

    Update hackertext.js

--- hackertext.js
+++ hackertext.js
@@ -24,7 +24,7 @@ window.onload = () => {
       }
 
       iterations += 1 / 3;
-    }, 100);
+    }, 50);
 
     function checkIfTextFinished() {
       const doneHeadings = document.querySelectorAll('.done');
